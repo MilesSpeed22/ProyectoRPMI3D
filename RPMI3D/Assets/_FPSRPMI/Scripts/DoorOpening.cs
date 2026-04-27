4255bc3da479ebd5d8b74f13c2a0e761547d8bb8
MilesSpeed22 <lorperdur@gmail.com>
2026-04-27 17:27:44 +0200

    mas cambios

--- RPMI3D/Assets/_FPSRPMI/Scripts/DoorOpening.cs
+++ RPMI3D/Assets/_FPSRPMI/Scripts/DoorOpening.cs
@@ -1,16 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorOpening : MonoBehaviour
 {
     [SerializeField] GameObject door;
-    [SerializeField] float open = 90f;
+    [SerializeField] float openAngle = 90f;
     [SerializeField] float speed = 2f;
+    [SerializeField] float waitTime = 2f;
+    
     bool isOpening;
+    bool isClosing;
+
     Quaternion targetRotation;
+    private Quaternion initialRotation;
 
     private void Start()
     {
-        targetRotation = Quaternion.Euler(0, open, 0) * door.transform.rotation;
+        initialRotation = door.transform.rotation;
+        targetRotation = Quaternion.Euler(0, openAngle, 0) * initialRotation;
     }
 
     private void Update()
@@ -18,7 +25,15 @@ public class DoorOpening : MonoBehaviour
         if (isOpening)
         {
             door.transform.rotation = Quaternion.Lerp(door.transform.rotation, targetRotation, Time.deltaTime * speed);
+
+            if (Quaternion.Angle(door.transform.rotation, targetRotation) < 0.1f)
+            {
+                isOpening = false;
+                StartCoroutine(DoorClosing());
+            }
         }
+
+        if (isClosing) door.transform.rotation = Quaternion.Lerp(door.transform.rotation, initialRotation, Time.deltaTime * speed);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -28,5 +43,10 @@ public class DoorOpening : MonoBehaviour
         }
     }
 
+    IEnumerator DoorClosing()
+    {
+        yield return new WaitForSeconds(waitTime);
+        isClosing = true;
+    }
 
 }
