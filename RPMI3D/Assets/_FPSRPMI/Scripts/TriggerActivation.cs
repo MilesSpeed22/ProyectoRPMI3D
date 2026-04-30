af378dc0ba348f327e0f652f9a5336551c3a24aa
MilesSpeed22 <lorperdur@gmail.com>
2026-04-30 15:08:53 +0200

    cambios locos

--- RPMI3D/Assets/_FPSRPMI/Scripts/TriggerActivation.cs
+++ RPMI3D/Assets/_FPSRPMI/Scripts/TriggerActivation.cs
@@ -4,12 +4,19 @@ public class TriggerActivation : MonoBehaviour
 {
     [SerializeField] GameObject actObj;
     [SerializeField] GameObject deacObj;
+    [SerializeField] GameObject enemyDeac;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             actObj.SetActive(true);
             deacObj.SetActive(false);
+            enemyDeac.SetActive(false);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        enemyDeac.SetActive(true);
     }
 }
